b3bd75bec3cb0de615910525b73a306b165c0524
1581033 <1582061033@qq.com>
2022-02-16 10:22:28 +0800

    1

--- matthiolaincana-ser/src/main/java/com/zhao/www/controller/sys/SysDictTypeController.java
+++ matthiolaincana-ser/src/main/java/com/zhao/www/controller/sys/SysDictTypeController.java
@@ -13,6 +13,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author Matthiola incana
  * @create 2022/1/22 10:54
@@ -54,6 +56,18 @@ public class SysDictTypeController {
     @PostMapping("/updataSysDictType")
     public ServiceResult<?> updataSysDictType(@RequestBody SysDictTypeParam param){
         return sysDictTypeService.updataSysDictType(param);
+    }
+
+    @ApiOperation(value = "上传菜单", notes = "上传菜单")
+    @PostMapping("/uploadSysDictType")
+    public ServiceResult<?> uploadSysDictType(@RequestBody SysDictTypeParam param){
+        return sysDictTypeService.uploadSysDictType(param);
+    }
+
+    @ApiOperation(value = "下载菜单", notes = "下载菜单")
+    @PostMapping("/downloadSysDictType")
+    public void downloadSysDictType(@RequestBody SysDictTypeParam param, HttpServletResponse response){
+        sysDictTypeService.downloadSysDictType(param,response);
     }
 
     @ApiOperation(value = "获取字典数据", notes = "获取字典数据")
