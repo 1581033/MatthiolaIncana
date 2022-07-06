1febca1a4e954c8f2e58acabf1590b7c7ae4a032
1581033 <1582061033@qq.com>
2022-07-06 11:59:22 +0800

    1

---
+++ src/config/style.ts
@@ -0,0 +1,16 @@
+export default {
+  showFooter: true,
+  isSidebarCompact: false,
+  showBreadcrumb: false,
+  mode: 'light',
+  layout: 'side',
+  splitMenu: false,
+  isFooterAside: false,
+  isSidebarFixed: true,
+  isHeaderFixed: true,
+  isUseTabsRouter: false,
+  showHeader: true,
+  backgroundTheme: 'blueGrey',
+  brandTheme: 'default',
+  size: 'medium',
+};
