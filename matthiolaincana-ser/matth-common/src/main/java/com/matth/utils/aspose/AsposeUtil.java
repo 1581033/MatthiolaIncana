0847e174fa48a60078fd523408f2dacbea002367
1581033 <1582061033@qq.com>
2022-08-15 09:20:30 +0800

    更新word文档工具类

--- matthiolaincana-ser/matth-common/src/main/java/com/matth/utils/aspose/AsposeUtil.java
+++ matthiolaincana-ser/matth-common/src/main/java/com/matth/utils/aspose/AsposeUtil.java
@@ -5,26 +5,27 @@ import com.aspose.cells.License;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class AsposeUtil {
 
     private static final String LICENSE = "license/aspose.cells.xml";
 
+    private static final InputStream STREAM = AsposeUtil.class.getClassLoader().getResourceAsStream(LICENSE);
+
     public static void cellsLicaspose() {
         License license = new License();
-        ClassPathResource classPathResourceCell = new ClassPathResource(LICENSE);
         try {
-            license.setLicense(classPathResourceCell.getInputStream());
-        } catch (IOException e) {
+            license.setLicense(STREAM);
+        } catch (Exception e) {
             System.out.println("Aspose注册异常" + e.getMessage());
         }
     }
 
     public static void wordsLicaspose() {
         com.aspose.words.License license = new com.aspose.words.License();
-        ClassPathResource classPathResourceCell = new ClassPathResource(LICENSE);
         try {
-            license.setLicense(classPathResourceCell.getInputStream());
+            license.setLicense(STREAM);
         } catch (Exception e) {
             System.out.println("Aspose注册异常" + e.getMessage());
         }
