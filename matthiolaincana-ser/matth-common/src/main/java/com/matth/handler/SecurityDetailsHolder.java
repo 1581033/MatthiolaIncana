85b542475ef7093f7eefdb96f7f8d763e160148e
1581033 <1582061033@qq.com>
2023-03-20 09:00:30 +0800

    更新word文档工具类

--- matthiolaincana-ser/matth-common/src/main/java/com/matth/handler/SecurityDetailsHolder.java
+++ matthiolaincana-ser/matth-common/src/main/java/com/matth/handler/SecurityDetailsHolder.java
@@ -3,7 +3,10 @@ package com.matth.handler;
 import com.matth.entity.model.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author Matthiola incana
@@ -15,8 +18,8 @@ public class SecurityDetailsHolder {
         return Optional.ofNullable((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).orElseThrow(NullPointerException::new);
     }
 
-    public static Object[] roles() {
-        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().toArray();
+    public static List<String> roles() {
+        return Arrays.stream(SecurityContextHolder.getContext().getAuthentication().getAuthorities().toArray()).map(Object::toString).collect(Collectors.toList());
     }
 
 }
